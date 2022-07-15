279cf7dcbd3192dd29070d0b9a8087cc799483ad
ValentinGhibaudo <vatghibaudo@gmail.com>
2022-07-15 17:59:20 +0200

    maj hrv

--- ghibtools/hrv.py
+++ ghibtools/hrv.py
@@ -24,3 +24,40 @@ def ecg_to_hrv(ecg, srate, show = False, inverse_sig = True):
     fci = 60 / interpolated_hrv
     
     return clean, fci
+
+def ecg_peaks(ecg, srate):
+    _,info = nk.ecg_peaks(ecg, sampling_rate=srate)
+    return info['ECG_R_Peaks']
+
+def peaks_to_RRI(peaks, srate):
+    peaks_time = peaks / srate
+
+    RRIs = []
+    for i, time in enumerate(peaks_time):
+        if i != 0:
+            RRIs.append(time - peaks_time[i-1])
+    return np.array(RRIs)*1000
+
+def RRI_to_successive_differences(RRIs):
+    successive_differences = []
+    for i, RRI in enumerate(RRIs):
+        if i != 0:
+            successive_differences.append(RRIs[i-1] - RRI)
+    return 
+
+def MeanNN(RRIs):
+    return np.mean(RRIs)
+
+def SDNN(RRIs):
+    return np.std(RRIs)
+
+def RMSSD(RRIs):
+    square_of_successive_differences = []
+    for i, RRI in enumerate(RRIs):
+        if i != 0:
+            square_of_successive_differences.append((RRIs[i-1] - RRI)**2)
+        
+    return np.sqrt(np.mean(square_of_successive_differences))
+
+def pNN50(RRIs):
+    return (sum(RRIs) > 50) / RRIs.size
