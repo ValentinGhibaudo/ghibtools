5ca3bd7014354b36b0cce8e1e973f08a04fd3ed3
ValentinGhibaudo <vatghibaudo@gmail.com>
2023-12-15 17:19:26 +0100

    correct n scr per min compute

--- ghibtools/eda.py
+++ ghibtools/eda.py
@@ -2,13 +2,15 @@ import neurokit2 as nk
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def get_eda_metrics(eda_signal, show = False):
-    df, info = nk.eda_process(eda_signal, sampling_rate=1000, method='neurokit')
+def get_eda_metrics(eda_signal, srate, show = False):
+    duration_in_secs =  eda_signal.size / srate
+    duration_in_mins = duration_in_secs / 60
+    df, info = nk.eda_process(eda_signal, sampling_rate=srate, method='neurokit')
     tonic = df['EDA_Tonic'].mean()
     info_df = pd.DataFrame.from_dict(info, orient = 'columns')
     n_scr = info_df.shape[0]
     metrics = pd.DataFrame.from_dict(info, orient = 'columns').drop(columns = ['sampling_rate','SCR_Onsets','SCR_Peaks','SCR_Recovery']).mean().to_frame().T
-    metrics.insert(0, 'N_SCR_per_min', n_scr)
+    metrics.insert(0, 'N_SCR_per_min', n_scr / duration_in_mins)
     metrics.insert(0, 'Tonic', tonic)
 
     if show:
