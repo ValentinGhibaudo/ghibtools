2ebf7962e364eeb77f1cbb0b4e03e150acf807a8
ValentinGhibaudo <vatghibaudo@gmail.com>
2024-03-13 10:58:11 +0100

    add time ratio year function to compute calendar dates from ratio like phylogeny dates

--- ghibtools/data_manip.py
+++ ghibtools/data_manip.py
@@ -26,3 +26,18 @@ def attribute_subplots(element_list, nrows, ncols):
             subplots_pos[f'{element_list[counter]}'] = [r,c]
             counter += 1
     return subplots_pos  
+
+def time_ratio_year(num, den, mode = 'from'):
+    """
+    num : int or float numerator in the ratio
+    den : int or float denominator in the ratio
+    mode : str ("from" compute date with 1 - ratio, "to" compute date with ratio
+    """
+    assert mode in ['from','to'], 'Mode should be "from" or "to"'
+    date_vector = pd.date_range(start = '2000-01-01', end = '2001-01-01', freq = '30s')
+    if mode == 'from':
+        ratio = 1 - (num / den)
+    elif mode == 'to':
+        ratio = num / den
+    ind_date = int(date_vector.size*(ratio))
+    return date_vector[ind_date]
